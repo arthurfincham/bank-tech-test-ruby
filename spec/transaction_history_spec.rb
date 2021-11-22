87b2f17065694ecf2e2e8028269586663d1f7f13
Arthur Fincham <arthurfincham@me.com>
2021-11-22 09:12:10 +0000

    add spec for if a tran is deposit or withdrawal

--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -1,12 +1,26 @@
-require "transaction_history"
+require 'transaction_history'
 
 describe TransactionHistory do
-  let(:transaction) { double :transaction, balance: 10 }
+  let(:transaction){ double :transaction, balance: 10 }
+  let(:transaction_2){ double :transaction, balance: 15 }
 
-  describe ".add" do
-    it "adds transaction to memory" do
+  describe '.add' do
+  it 'adds transaction to memory' do
+    subject.add(transaction)
+    expect(subject.memory).to include(transaction)
+  end
+  end
+
+  describe '.is_a_deposit' do
+    it 'returns true if the transaction adds to balance' do
       subject.add(transaction)
-      expect(subject.memory).to include(transaction)
+      subject.add(transaction_2)
+      allow(transaction).to receive(:get_balance).and_return(10)
+      allow(transaction_2).to receive(:get_balance).and_return(15)
+      expect(subject.is_a_deposit(transaction_2)).to be true
     end
   end
+
+
+
 end
