cd73db308067bb66c203d1abe5fbc195ed252c05
Arthur Fincham <arthurfincham@me.com>
2021-11-22 09:12:37 +0000

    add is_a_deposit method, pass test

--- lib/transaction_history.rb
+++ lib/transaction_history.rb
@@ -9,4 +9,8 @@ class TransactionHistory
     @memory << tran
   end
 
+  def is_a_deposit(item)
+    i = @memory.find_index(item)
+    @memory[i].get_balance > @memory[i-1].get_balance || i == 0 ? true : false
+  end
 end
