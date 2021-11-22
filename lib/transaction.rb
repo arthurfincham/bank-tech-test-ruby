82f4ccdfb86444e64a65a3d78fbabb16d4ddc5fd
Arthur Fincham <arthurfincham@me.com>
2021-11-22 09:00:19 +0000

    pass tran getter test

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,12 @@
 class Transaction
+attr_reader :amount, :date, :balance
+  def initialize(amount, date, balance)
+    @amount = amount
+    @date = date
+    @balance = balance
+  end
   
+  def get_balance
+    @balance
+  end
 end
