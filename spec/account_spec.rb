acb4fd444d226e76a64daadad78fdc6f3b71b7f0
Arthur Fincham <arthurfincham@me.com>
2021-11-23 12:27:37 +0000

    test that methods are called for Account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,39 +1,39 @@
 require "account"
 require "date"
+
 describe Account do
 
   before(:all) do
     @mock_date = Date.new(2021,10,23)
   end
 
-  describe "initialisation" do
-    it "has a balance of 0" do
-      expect(subject.balance).to eq 0
-    end
-  end
-
   describe ".deposit" do
-    before do
-      subject.deposit(10, @mock_date)
+    it "updates the balance" do
+      expect{ subject.deposit(10, @mock_date) }.to change{subject.balance }.by (10)
     end
 
-    it "updates the balance" do
-      expect(subject.balance).to eq 10
+    it 'creates a new transcation' do
+      expect(subject).to receive(:new_transaction).with(10, @mock_date)
+      subject.deposit(10, @mock_date)
     end
   end
 
   describe ".withdraw" do
     before do
       subject.deposit(10, @mock_date)
-      subject.withdraw(5, @mock_date)
-    end
-
-    it "updates the balance" do
-      expect(subject.balance).to eq 5
     end
 
     it "throws error if insufficient funds" do
       expect { subject.withdraw(20, Date.new(2021, 10, 21)) }.to raise_error("Insufficient funds.")
+    end
+
+    it "updates the balance" do
+      expect{ subject.withdraw(5, @mock_date) }.to change{subject.balance }.by (-5)
+    end
+
+    it 'creates a new transcation' do
+      expect(subject).to receive(:new_transaction).with(5, @mock_date)
+      subject.withdraw(5, @mock_date)
     end
   end
 end
